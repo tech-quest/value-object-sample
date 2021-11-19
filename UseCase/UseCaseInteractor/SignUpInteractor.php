0809b79731ad954bdfdfe3437f111e72fe53f197
Mizuki Ohmiya <m.omiya@miwaza.co.jp>
2021-11-19 16:26:59 +0900

    メッセージを定数に変更

--- UseCase/UseCaseInteractor/SignUpInteractor.php
+++ UseCase/UseCaseInteractor/SignUpInteractor.php
@@ -2,6 +2,8 @@
 
 final class SignUpInteractor
 {
+  const ALLREADY_EXISTS_MESSAGE = "すでに登録済みのメールアドレスです";
+  const COMPLETED_MESSAGE = "登録が完了しました";
 
   private $useCaseInput;
 
@@ -16,10 +18,10 @@ final class SignUpInteractor
     $user = $userDao->findByMail($this->useCaseInput->email());
 
     if (!is_null($user)) {
-      return new SignUpOutput(false, "すでに登録済みのメールアドレスです");
+      return new SignUpOutput(false, self::ALLREADY_EXISTS_MESSAGE);
     }
 
     $userDao->create($this->useCaseInput->name(), $this->useCaseInput->email(), $this->useCaseInput->password());
-    return new SignUpOutput(true, "登録が完了しました");
+    return new SignUpOutput(true, self::COMPLETED_MESSAGE);
   }
 }
