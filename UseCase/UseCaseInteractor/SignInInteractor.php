64abc5eb06fa9b547bde220f1dc466b3170b85df
Akihiro Uchida <58426456+orihikadichu@users.noreply.github.com>
2021-11-22 09:20:43 +0900

    SignInInteractorのリファクタ

--- UseCase/UseCaseInteractor/SignInInteractor.php
+++ UseCase/UseCaseInteractor/SignInInteractor.php
@@ -4,26 +4,44 @@ final class SignInInteractor
 {
     const FAILED_MESSAGE = "メールアドレスまたは<br />パスワードが間違っています";
 
+    private $userDao;
     private $useCaseInput;
 
     public function __construct(SignInInput $useCaseInput)
     {
+        $this->userDao = new UserDao();
         $this->useCaseInput = $useCaseInput;
     }
 
     public function handler(): SignInOutput
     {
-        $userDao = new UserDao();
-        $user = $userDao->findByMail($this->useCaseInput->email());
+        $user = $this->findUser();
 
-        if (is_null($user)) {
+        if ($this->notExistsUser($user)) {
             return new SignInOutput(false, self::FAILED_MESSAGE);
         }
 
-        if (!password_verify($this->useCaseInput->password(), $user["password"])) {
+        if ($this->isInvalidPassword($user['password'])) {
             return new SignInOutput(false, self::FAILED_MESSAGE);
         }
+
+        $this->saveSession($user);
 
         return new SignInOutput(true, $user);
+    }
+
+    private function findUser(): array
+    {
+        return $userDao->findByMail($this->useCaseInput->email());
+    }
+
+    private function notExistsUser(?array $user): bool
+    {
+        return is_null($user);
+    }
+
+    private function isInvalidPassword(string $password): bool
+    {
+        return !password_verify($this->useCaseInput->password(), $password);
     }
 }
