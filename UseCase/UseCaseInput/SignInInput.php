97fcec9a4db139f7833ec4382ed72f13ff549dc0
Akihiro Uchida <58426456+orihikadichu@users.noreply.github.com>
2021-11-23 16:25:28 +0900

    コメントの追加

--- UseCase/UseCaseInput/SignInInput.php
+++ UseCase/UseCaseInput/SignInInput.php
@@ -1,21 +1,43 @@
 <?php
 
+/**
+ * ログインユースケースの入力値
+ */
 final class SignInInput
 {
+    /**
+     * @var Email
+     */
     private $email;
+
+    /**
+     * @var InputPassword
+     */
     private $password;
 
+    /**
+     * コンストラクタ
+     * 
+     * @param Email $email
+     * @param InputPassword $password
+     */
     public function __construct(Email $email, InputPassword $password)
     {
         $this->email = $email;
         $this->password = $password;
     }
 
+    /**
+     * @return Email
+     */
     public function email(): Email
     {
         return $this->email;
     }
 
+    /**
+     * @return InputPassword
+     */
     public function password(): InputPassword
     {
         return $this->password;
